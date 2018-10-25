aeceaf147d2dc4e9c116484021c769002c64c9c0
nhungdtmr <nhungdtmr@gmail.com>
2018-10-25 16:57:53 +0700

    First Fixbug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :set_locale
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
   def hello
     render html: "hello, world!"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-
-  	# get 'users/new'
-
-  	root 'static_pages#home'
-  	get  '/help',    to: 'static_pages#help', as: 'help'
-  	get  '/about',   to: 'static_pages#about'
-  	get  '/contact', to: 'static_pages#contact'
-    get  '/signup',  to: 'users#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # root 'application#hello'
+  	root "static_pages#home"
+  	get "/help", to: "static_pages#help", as: "help"
+  	get "/about", to: "static_pages#about"
+  	get "/contact", to: "static_pages#contact"
+    get "/signup", to: "users#new"
 end
