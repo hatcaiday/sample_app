4cd52003444cd76b0c6d80ed5509ebe0dbef5174
nhungdtmr <nhungdtmr@gmail.com>
2018-11-03 00:59:26 +0700

    First commit on branch sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   before_save {email.downcase!}
-  validates :name, presence: true, length: {maximum: Settings.maximum_name}
+  validates :name, presence: true, length: {maximum: Settings.value.maximum_name.to_i}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.maximum_email},
+  validates :email, presence: true, length: {maximum: Settings.value.maximum_email.to_i},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.maximum_pass}
+  validates :password, presence: true, length: {minimum: Settings.value.maximum_pass.to_i}
 end
