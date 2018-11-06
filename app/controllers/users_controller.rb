22c7819b2549c9b095d7a656a4a654952f9c783b
nhungdtmr <nhungdtmr@gmail.com>
2018-11-06 14:46:55 +0700

    First Fixbug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by id: params[:id]
   end
   
   def new
@@ -9,17 +8,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
-      flash[:success] = "users.welcome_to"
+      flash[:success] = t".users.welcome_to"
     else
-      render "new"
+      render :new
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def user_params
+    params.require(:user).permit :name, :email, :password, 
+    :password_confirmation
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   before_save {email.downcase!}
-  validates :name, presence: true, length: {maximum: Settings.value.maximum_name.to_i}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.value.maximum_email.to_i},
+  validates :name, presence: true, length: {maximum: Settings.value.maximum_name}
+  validates :email, presence: true, length: {maximum: Settings.value.maximum_email},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.value.maximum_pass.to_i}
+  validates :password, presence: true, length: {minimum: Settings.value.maximum_pass}
 end
